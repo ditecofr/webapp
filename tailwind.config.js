481e503564cdb8312a4d626f3797a9269ef05a8f
Thomas Eyaa <thomaseyaa.pro@gmail.com>
2025-02-02 14:57:41 +0100

    Configured Tailwind CSS locally and add PostCSS setup 🛠️

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['system-ui', 'sans-serif'],
+        jakarta: ['Plus Jakarta Sans', 'sans-serif'],
+      },
+      colors: {
+        'primary-green': '#2EAE60',
+        'primary-green-hover': '#259652',
+        'primary-blue': '#2E86DE',
+        'primary-blue-hover': '#2874C4',
+        'primary-dark': '#1B2A3B',
+        'primary-green-light': '#D4ECD9',
+      },
+    },
   },
   plugins: [],
 }
