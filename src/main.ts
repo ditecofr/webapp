371ef1b00226e662b6d1277b6150cd0e2872a89d
Thomas Eyaa <thomaseyaa.pro@gmail.com>
2025-02-02 14:58:35 +0100

    Added Plus Jakarta Sans font and global typography styles 🔤

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import './assets/main.css'
+import '@fontsource/plus-jakarta-sans/400.css'
+import '@fontsource/plus-jakarta-sans/500.css'
+import '@fontsource/plus-jakarta-sans/600.css'
+import '@fontsource/plus-jakarta-sans/700.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
