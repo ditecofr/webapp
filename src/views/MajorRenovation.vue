1554ff1779873c2d2bec089c24a370a07f33040f
Thomas Eyaa <thomaseyaa.pro@gmail.com>
2025-02-02 15:03:08 +0100

    Redesigned MajorRenovation hero and benefits sections

--- src/views/MajorRenovation.vue
+++ src/views/MajorRenovation.vue
@@ -1,120 +1,99 @@
 <template>
   <div>
-    <div class="container mx-auto px-8">
-      <Header />
+    <div class="bg-primary-green">
+      <div class="container mx-auto px-8 pb-12">
+        <Header />
 
-      <!-- Hero -->
-      <div class="flex flex-col lg:flex-row lg:mt-16 items-center gap-8">
-        <div class="w-full lg:w-[50%]">
-          <h1
-            class="text-4xl md:text-5xl lg:text-6xl font-bold mb-8 lg:mb-10 leading-tight bg-gradient-to-r from-[#56B476] to-[#2C7688] bg-clip-text text-transparent"
-          >
-            Transformez votre logement avec MaPrimeRenov
-          </h1>
-          <div class="flex flex-col mb-8 lg:mb-10 gap-6">
-            <p class="text-xl md:text-2xl font-medium">
-              Passez à une Classe Énergétique Supérieure avec Jusqu'à 63 000 € d'Aides
-            </p>
-            <p class="w-full lg:w-3/4 text-base lg:text-lg leading-relaxed text-gray-600">
-              Avec Diteco, améliorez la performance énergétique de votre logement et bénéficiez d'un
-              accompagnement personnalisé pour réduire vos factures.
-            </p>
-          </div>
-        </div>
-
-        <div class="relative w-full lg:w-[45%] lg:mt-12">
-          <div
-            class="absolute -z-10 w-[100%] h-[600px] bg-gradient-to-r from-[#56B476]/20 to-[#2C7688]/20 rounded-full blur-3xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-          ></div>
-          <EligibilityForm class="backdrop-blur-sm bg-white/80 rounded-3xl shadow-2xl p-8" />
-        </div>
-      </div>
-    </div>
-
-    <div class="flex flex-col lg:flex-row mt-20 bg-[#DAEADF]">
-      <div class="w-full lg:w-[50%] rounded-none lg:rounded-tr-[250px] bg-[#56B476]">
-        <div class="container flex h-full mx-auto px-8 md:px-20 py-12">
-          <div class="self-end text-white">
-            <h1 class="text-3xl font-bold">
-              Des Aides Financières <br />
-              Exceptionnelles : <br /><br />
-              Jusqu'à 63 000 €
+        <!-- Hero -->
+        <div class="flex flex-col lg:flex-row lg:mt-16 items-center justify-between">
+          <div class="w-full lg:w-[60%] text-white text-center lg:text-left pt-8 lg:pt-0">
+            <h1 class="text-4xl lg:text-7xl font-bold font-jakarta mb-8 lg:mb-10">
+              Donnez<br class="block lg:hidden" />
+              une nouvelle vie à votre logement
             </h1>
+            <div class="flex flex-col mb-8 lg:mb-10 gap-6">
+              <p class="text-xl md:text-2xl">
+                Jusqu'à 63 000 € d'aides pour améliorer votre Classe Énergétique.
+                <br class="hidden lg:block" />Diteco vous accompagne pour réduire vos factures
+              </p>
+            </div>
           </div>
-        </div>
-      </div>
 
-      <div class="w-full lg:w-[55%]">
-        <div class="container mx-auto px-8 md:px-20 lg:ps-40 py-12">
-          <h1 class="text-3xl font-bold mb-6">
-            Pourquoi choisir le programme <br />
-            rénovation d'ampleur ?
-          </h1>
-          <p class="text-lg">
-            Ce programme soutient vos projets de rénovation globale en récompensant les sauts de
-            classe énergétique. Plus vous améliorez votre logement, plus les aides augmentent. Un
-            Accompagnement sur-mesure avec mon accompagnateur rénov', un expert dédié à vos côtés
-            pour chaque étape, depuis le diagnostic initial jusqu'à la validation post-travaux.
-            Réduisez vos factures d'énergie de 30 % à 70 %, améliorez votre confort et valorisez la
-            valeur de votre bien immobilier.
-          </p>
+          <div class="relative w-full lg:w-[40%] lg:mt-12">
+            <EligibilityForm />
+          </div>
         </div>
       </div>
     </div>
 
     <!-- Benefits -->
-    <div id="benefits" class="container mx-auto px-8 pt-12 mt-12">
+    <div id="benefits" class="container mx-auto px-8 pt-24">
       <div class="flex flex-col items-center">
-        <h1
-          class="text-4xl font-bold text-center mb-14 bg-gradient-to-r from-[#56B476] to-[#2C7688] bg-clip-text text-transparent"
-        >
-          Les bénéfices d'un saut de classe énergétique
+        <h1 class="text-4xl text-center lg:text-6xl font-bold font-jakarta pb-10 lg:pb-14">
+          Pourquoi opter pour<br class="hidden lg:block" />
+          la rénovation d'ampleur ?
         </h1>
 
         <div class="flex flex-col lg:flex-row justify-between gap-10 lg:gap-20">
-          <div class="flex h-full flex-1 flex-col items-center gap-4 lg:gap-10 text-center">
-            <TrendingDown class="text-[#56B476]" :size="100" />
+          <div class="flex h-full flex-1 flex-col items-center gap-4 lg:gap-6 text-center">
+            <img
+              src="@/assets/illustrations/Savings.gif"
+              class="size-[250px] scale-x-[-1]"
+              alt="Illustration économies d'énergie"
+            />
             <div>
-              <h2 class="text-xl lg:text-2xl font-bold mb-4">Réduction de dépenses énergétiques</h2>
+              <h2 class="text-2xl lg:text-3xl font-bold mb-4">Économies d'énergie</h2>
               <p>
-                Température stable, meilleure isolation acoustique et qualité de l'air améliorée.
+                Passez de la classe G ou F à une classe D, C ou mieux et économisez jusqu'à 70 % sur
+                vos factures
               </p>
             </div>
           </div>
 
-          <div class="flex h-full flex-1 flex-col items-center gap-4 lg:gap-10 text-center">
-            <div class="h-[108px] lg:h-[156px]">
-              <leaf class="text-[#56B476]" :size="100" />
-            </div>
+          <div class="flex h-full flex-1 flex-col items-center gap-4 lg:gap-6 text-center">
+            <img
+              src="@/assets/illustrations/Comfort.gif"
+              class="size-[250px] scale-x-[-1]"
+              alt="Illustration confort"
+            />
             <div>
-              <h2 class="text-xl lg:text-2xl font-bold mb-4">Un confort amélioré</h2>
+              <h2 class="text-2xl lg:text-3xl font-bold mb-4">Un confort amélioré</h2>
               <p>
-                Température stable, meilleure isolation acoustique et qualité de l'air améliorée.
+                Température stable, meilleure isolation acoustique et qualité de l'air améliorée
               </p>
             </div>
           </div>
 
-          <div class="flex h-full flex-1 flex-col items-center gap-4 lg:gap-10 text-center">
-            <div class="h-[108px] lg:h-[156px]">
-              <ShieldCheck class="text-[#56B476]" :size="100" />
-            </div>
+          <div class="flex h-full flex-1 flex-col items-center gap-4 lg:gap-6 text-center">
+            <img
+              src="@/assets/illustrations/Insurance.gif"
+              class="size-[250px]"
+              alt="Illustration confort"
+            />
             <div>
-              <h2 class="text-xl lg:text-2xl font-bold mb-4">Conformité avec la réglementation</h2>
+              <h2 class="text-2xl lg:text-3xl font-bold mb-4">Conformité réglementaire</h2>
               <p>
                 Les logements classés G ou F ne pourront bientôt plus être loués. Prenez de l'avance
-                dès maintenant.
+                dès maintenant
               </p>
             </div>
           </div>
         </div>
       </div>
+
+      <!-- <p class="text-lg">
+        Ce programme soutient vos projets de rénovation globale en récompensant les sauts de classe
+        énergétique. Plus vous améliorez votre logement, plus les aides augmentent. Un
+        Accompagnement sur-mesure avec mon accompagnateur rénov', un expert dédié à vos côtés pour
+        chaque étape, depuis le diagnostic initial jusqu'à la validation post-travaux. Réduisez vos
+        factures d'énergie de 30 % à 70 %, améliorez votre confort et valorisez la valeur de votre
+        bien immobilier.
+      </p> -->
     </div>
 
     <!-- Examples -->
     <div class="container flex flex-col items-center mx-auto pt-12 mt-12 px-8">
-      <h1
-        class="text-4xl font-bold text-center mb-14 bg-gradient-to-r from-[#56B476] to-[#2C7688] bg-clip-text text-transparent"
-      >
+      <h1 class="text-4xl font-bold text-center mb-14 text-primary-green">
         Exemples concrets de rénovations avec le programme
       </h1>
 
@@ -134,7 +113,7 @@
                 <p><strong>Aides obtenues :</strong> 38 000 €</p>
                 <p><strong>Économies réalisées :</strong> -55 % sur les factures</p>
               </div>
-              <h2 class="text-xl font-bold p-2 mt-5 mb-4 bg-[#2C7688] text-white">
+              <h2 class="text-xl font-bold p-2 mt-5 mb-4 bg-primary-green text-white">
                 De classe G à classe D
               </h2>
             </div>
@@ -148,7 +127,7 @@
                 <p><strong>Aides obtenues :</strong> 50 000 €</p>
                 <p><strong>Économies réalisées :</strong> -70 % sur les factures</p>
               </div>
-              <h2 class="text-xl font-bold p-2 mt-5 mb-4 bg-[#2C7688] text-white">
+              <h2 class="text-xl font-bold p-2 mt-5 mb-4 bg-primary-green text-white">
                 De classe F à classe B
               </h2>
             </div>
@@ -163,7 +142,7 @@
                 </p>
                 <p><strong>Aides obtenues :</strong> 63 000 € cumulées</p>
               </div>
-              <h2 class="text-xl font-bold p-2 mt-5 mb-4 bg-[#2C7688] text-white">
+              <h2 class="text-xl font-bold p-2 mt-5 mb-4 bg-primary-green text-white">
                 De classe G à classe C
               </h2>
             </div>
@@ -228,9 +207,9 @@
       <h1 class="mb-14 text-3xl text-center font-bold">Êtes-vous Éligible au Programme ?</h1>
 
       <div class="flex flex-col md:flex-row mb-14">
-        <div class="flex flex-col items-center p-8 md:my-8 md:w-[250px] bg-[#DAEADF]">
+        <div class="flex flex-col items-center p-8 md:my-8 md:w-[250px] bg-primary-green-light">
           <div class="bg-white rounded-full p-4 mb-4">
-            <House class="text-[#56B476]" :size="56" />
+            <House class="text-primary-green" :size="56" />
           </div>
           <p class="text-center">
             Logement construit<br />
@@ -238,10 +217,10 @@
           </p>
         </div>
 
-        <div class="flex items-center p-8 md:p-12 bg-[#56B476]">
+        <div class="flex items-center p-8 md:p-12 bg-primary-green">
           <div class="flex flex-col items-center">
             <div class="bg-white rounded-full p-4 mb-4">
-              <ChartSpline class="text-[#56B476]" :size="56" />
+              <ChartSpline class="text-primary-green" :size="56" />
             </div>
             <p class="text-center text-lg text-white">
               Saut d'au moins 2 classes<br />
@@ -250,9 +229,9 @@
           </div>
         </div>
 
-        <div class="flex flex-col items-center p-8 md:my-8 md:w-[250px] bg-[#DAEADF]">
+        <div class="flex flex-col items-center p-8 md:my-8 md:w-[250px] bg-primary-green-light">
           <div class="bg-white rounded-full p-4 mb-4">
-            <List class="text-[#56B476]" :size="56" />
+            <List class="text-primary-green" :size="56" />
           </div>
           <p class="text-center">
             Revenus respectant les<br />
@@ -263,7 +242,7 @@
 
       <a
         href="#"
-        class="inline-block rounded-full px-8 py-3.5 bg-[#56B476] text-white hover:bg-[#45a065]"
+        class="inline-block rounded-full px-8 py-3.5 bg-primary-green text-white hover:bg-primary-green-hover"
       >
         Vérifiez gratuitement votre éligibilité
       </a>
