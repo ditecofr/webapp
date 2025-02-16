edb4eede6bd0e33098f51ec8e1cd9cb805973fe6
Thomas Eyaa <thomaseyaa.pro@gmail.com>
2025-02-16 22:29:38 +0100

    Added Vercel Analytics for tracking and performance insights 📊

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,14 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { createPinia } from 'pinia'
+import { inject } from '@vercel/analytics'
+
 import './assets/main.css'
 import '@fontsource/plus-jakarta-sans/400.css'
 import '@fontsource/plus-jakarta-sans/500.css'
 import '@fontsource/plus-jakarta-sans/600.css'
 import '@fontsource/plus-jakarta-sans/700.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(createPinia()).use(router).mount('#app')
+inject()
