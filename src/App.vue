9bf266202dcb6b8772f3c797dee4bd1213f5f7e5
Thomas Eyaa <thomaseyaa.pro@gmail.com>
2025-02-21 00:45:14 +0100

    Added dynamic page title 🏷️

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,26 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import { computed, watch } from 'vue'
+
+const router = useRouter()
+const isOnRenovationDampleur = computed(
+  () =>
+    router.currentRoute.value.path === '/' ||
+    router.currentRoute.value.path === '/renovation-dampleur',
+)
+
+const title = computed(() =>
+  isOnRenovationDampleur.value ? "Diteco - Rénovation D'ampleur" : 'Diteco - Pompe à chaleur',
+)
+
+watch(
+  title,
+  (newTitle) => {
+    document.title = newTitle
+  },
+  { immediate: true },
+)
+</script>
